c18e36bbaf53767bac6e063d640ce5acdb9a32f2
chaeyounsong <chaeyounsong@gmail.com>
2019-06-19 23:12:02 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-628bcc0aab239c798d66.js"
+    "url": "webpack-runtime-46acae66ea148930cdd8.js"
   },
   {
     "url": "styles.0967b04e1b8dd0617d52.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3dd97e3743d8f19b2af3881f2c7612fb"
+    "revision": "3cc91fd7c01aa440854d3a258f1bd2cb"
   },
   {
     "url": "manifest.json",
